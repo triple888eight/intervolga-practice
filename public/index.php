b9f9bb9477810ccfd1b3bf89931286da553db926
razdevaysya <sinko02@inbox.ru>
2022-11-30 17:26:47 +0300

    Сделал 2 контроллера

--- public/index.php
+++ public/index.php
@@ -1,3 +1,31 @@
 <?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Slim\Exception\NotFoundException;
 
 require __DIR__ . '/../vendor/autoload.php'; // Автозагрузка
+
+$app = AppFactory::create();
+
+$app->setBasePath("/composer/public/index.php"); // Указываю базовый путь, иначе ошибка
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name!");
+    return $response;
+});
+
+// try {
+//     $app->run();     
+// } catch (Exception $e) {    
+//     // We display a error message
+//     die( json_encode(array("status" => "failed", "message" => "This action is not allowed"))); 
+// }
+
+$app->run();
