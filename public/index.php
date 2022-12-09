d67e80c1736fe01c8aef0e4bd7734a959df9aef3
razdevaysya <sinko02@inbox.ru>
2022-12-09 03:40:28 +0300

    Использую контроллеры для вывода и добавления отзывов

--- public/index.php
+++ public/index.php
@@ -8,7 +8,6 @@ use Tuupola\Middleware\HttpBasicAuthentication as Auth;
 use App\Connection;
 use App\reviewStorage;
 use App\Config;
-use App\Controllers\HelloController;
 
 require __DIR__ . '/../vendor/autoload.php'; // Автозагрузка
 
@@ -20,83 +19,28 @@ $container = $app->getContainer();
 // Hello world
 $app->get('/hello', \App\Controllers\HelloController::class . ':hello');
 
-// Hello {name}
+// Hello, {name}
 $app->get('/hello/{name}', \App\Controllers\HelloController::class . ':helloName');
 
-// Получение отзыва
-$app->get('/api/feedbacks/{id}/', function (Request $request, Response $response, array $args){
-    /*header('Content-type: application/json; charset=utf-8');*/
-
-    $pdo = Connection::connect(); // Подключение к БД
-
-    $sqlite = new reviewStorage;
-    $id = (int)$args['id'];
-
-    $result = $sqlite->getReviewById($id, $pdo);
-
-    $response->getBody()->write($result);
-
-    // из документации
-    /*return $response->withHeader('content-type','application/json');*/
-    return $response;
-});
+// Получение отзыва по id
+$app->get('/api/feedbacks/{id}', \App\Controllers\FeedbackController::class . ':getFeedbackById');
 
 // Постраничный вывод отзывов, страницы указывается как page=...
-$app->get('/api/feedbacks/', function (Request $request, Response $response, array $args) {
-    /*header('Content-type: application/json; charset=utf-8');*/
+$app->get('/api/feedbacks/', \App\Controllers\FeedbackController::class . ':getFeedbacksPageByPage');
 
-    $pdo = Connection::connect(); // Подключение к БД
-    $sqlite = new reviewStorage;
-
-    // Берем значение page из GET запроса, если его нет, то выводится 1 страница
-    if($_GET) $page = $_GET['page'];
-    else $page = 1;
-
-    // Вызывается метод из reviewStorage
-    $result = $sqlite->getNavReviews($page, $pdo);
-
-    /*$array = json_decode($result);
-    print_r($array);*/
-
-    echo $result;
-
-    /*$response->getBody()->write($result);*/
-
-    // из документации
-    /*return $response->withHeader('content-type','application/json');*/
-
-    if (!$response){
-        return $response->withStatus(400);
-    }
-
-    return $response;
-});
-
-// Отображения страницы добавления пользователя
-$app->get('/add', function (Request $request, Response $response){
+// Отображение страницы для добавления пользователя
+$app->get('/api/add', function (Request $request, Response $response){
     header('Content-type: text/html; charset=utf-8');
+
     $renderer = new PhpRenderer("../templates");
-    return $renderer->render($response,"add_review.php");
+    return $renderer->render($response,"add_review_js.php");
 });
 
-// Отправка запроса в БД
-$app->post('/adding', function (Request $request, Response $response){
+// Добавление отзыва с помощью Js и AJAX
+$app->post('/api/adding', \App\Controllers\AddingController::class . ':AddingReviewByJs');
 
-    $pdo = Connection::connect(); // Подключение к БД
 
-    $sqlite = new reviewStorage;
-
-    // Получаю значения с формы в массив
-    $data = $request->getParsedBody();
-
-    $result = $sqlite->addReview($pdo, $data);
-    $response->getBody()->write($result);
-
-    /*print_r($data);*/
-    return $response;
-});
-
-// Аутентификация пользователя, защита для страницы удаления пользователей по адресу /delete
+// Basic аутентификация
 $app->add(new Auth([
     "path" => "/delete",
     "realm" => "Protected",
@@ -127,33 +71,11 @@ $app->post('/deleting', function (Request $request, Response $response) {
     return $response;
 });
 
-$app->get('/api/add', function (Request $request, Response $response){
-    header('Content-type: text/html; charset=utf-8');
-
-    $renderer = new PhpRenderer("../templates");
-    return $renderer->render($response,"add_review_js.php");
-});
-
-$app->post('/api/adding', function (Request $request, Response $response){
-    $pdo = Connection::connect(); // Подключение к БД
-
-    $sqlite = new reviewStorage;
-
-    // Получаю значения с формы в массив
-    $data = $request->getParsedBody();
-
-    $result = $sqlite->addReviewByJs($pdo, $data);
-
-    $response->getBody()->write(json_encode($result));
-
-    return $response;
-});
-
 /*$app->run();*/
 
 try {
-     $app->run();
- } catch (Exception $e) {
-     // Сообщение об ошибке
-     die( json_encode(array("status" => "failed", "message" => "This action is not allowed")));
- }
+    $app->run();
+} catch (Exception $e) {
+    // Сообщение об ошибке
+    die( json_encode(array("status" => "failed", "message" => "This action is not allowed")));
+}
