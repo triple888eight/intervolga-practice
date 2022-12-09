45abd14824b757f56b955c8bc6b7d0c48e45cf9a
razdevaysya <sinko02@inbox.ru>
2022-12-09 04:21:05 +0300

    Добавил папку views и файл views.php для отображения страниц

--- public/index.php
+++ public/index.php
@@ -1,11 +1,6 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-use Slim\Views\PhpRenderer;
-use Tuupola\Middleware\HttpBasicAuthentication as Auth;
-use App\DB\Config;
 
 require __DIR__ . '/../vendor/autoload.php'; // Автозагрузка
 
@@ -15,39 +10,10 @@ $app = AppFactory::create();
 
 require __DIR__ . '/../routes/routes.php';
 
-// Отображение страницы для добавления пользователя
-$app->get('/api/add', function (Request $request, Response $response){
-    header('Content-type: text/html; charset=utf-8');
-
-    $renderer = new PhpRenderer("../templates");
-    return $renderer->render($response,"add_review_js.php");
-});
-
-// Добавление отзыва с помощью Js и AJAX
-$app->post('/api/adding', \App\Controllers\AddingController::class . ':AddingReviewByJs');
-
-// Basic аутентификация
-$app->add(new Auth([
-    "path" => "/delete",
-    "realm" => "Protected",
-    "users" => [
-        Config::logAdmin => Config::passAdmin
-    ]
-]));
-
-// Отображение страницы удаления
-$app->get('/admin/delete', function (Request $request, Response $response){
-    header('Content-type: text/html; charset=utf-8');
-
-    $renderer = new PhpRenderer("../templates");
-    return $renderer->render($response,"delete_review.php");
-});
-
-$app->post('/admin/deleting', \App\Controllers\DeletingController::class . ':deleteReviewById');
+require __DIR__ . '/../views/views.php';
 
 try {
     $app->run();
 } catch (Exception $e) {
-    // Сообщение об ошибке
-    die( json_encode(array("status" => "failed", "message" => "This action is not allowed")));
+    echo "Адрес не поддерживается";
 }
