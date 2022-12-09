0720d7e5dbd0cef004bad5800e423b3a8e3465c2
razdevaysya <sinko02@inbox.ru>
2022-12-09 05:14:42 +0300

    Добавил файл README.md

--- routes/routes.php
+++ routes/routes.php
@@ -7,7 +7,7 @@ $app->get('/hello', \App\Controllers\HelloController::class . ':hello');
 $app->get('/hello/{name}', \App\Controllers\HelloController::class . ':helloName');
 
 // Получение отзыва по id
-$app->get('/api/feedbacks/{id}', \App\Controllers\FeedbackController::class . ':getFeedbackById');
+$app->get('/api/feedbacks/{id}/', \App\Controllers\FeedbackController::class . ':getFeedbackById');
 
 // Постраничный вывод отзывов, страницы указывается как page=...
 $app->get('/api/feedbacks/', \App\Controllers\FeedbackController::class . ':getFeedbacksPageByPage');
